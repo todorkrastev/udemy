c25e7c771b1a70240485ce76cdaf731a0119c6cd
beinsaduno <krastevtodordinev@yahoo.com>
2022-11-02 11:47:39 +0100

    Add assignment

---
+++ angular/S05_Components&DataBindingDeepDive/assignment/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameControlComponent,
+    EvenComponent,
+    OddComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
