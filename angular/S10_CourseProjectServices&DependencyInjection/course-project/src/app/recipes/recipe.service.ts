199f6201016439db3df51d7878da2d8818b63eec
beinsaduno <krastevtodordinev@yahoo.com>
2022-11-07 00:36:12 +0100

    Add S10_CourseProjectServices&DependencyInjection

---
+++ angular/S10_CourseProjectServices&DependencyInjection/course-project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1920px-Hamburger_%28black_bg%29.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
