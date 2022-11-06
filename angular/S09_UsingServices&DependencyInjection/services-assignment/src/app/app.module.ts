2ba7bb2f8f4fbff70e138387a269a28545e1a6fa
beinsaduno <krastevtodordinev@yahoo.com>
2022-11-06 17:22:44 +0100

    Add services-assignment

---
+++ angular/S09_UsingServices&DependencyInjection/services-assignment/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { CounterService } from './counter.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InactiveUsersComponent,
+    ActiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [CounterService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
