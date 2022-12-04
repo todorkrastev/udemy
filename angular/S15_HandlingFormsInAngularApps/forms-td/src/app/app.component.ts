a08c9351f7c30f7f27f79a6eb4f986c2b20edb60
beinsaduno <krastevtodordinev@yahoo.com>
2022-12-03 21:03:51 -1200

    Add S15_HandlingFormsInAngularApps Update S15_HandlingFormsInAngularApps#assignment
    C:
    dir

---
+++ angular/S15_HandlingFormsInAngularApps/forms-td/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
