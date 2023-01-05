7e2c90b8c35d200582b9e45788df1f19ae051797
beinsaduno <krastevtodordinev@yahoo.com>
2023-01-05 04:26:18 +0100

    Create and Setup Project

---
+++ spring/master-spring-data-jpa-with-hibernate/src/main/java/com/todorkrastev/masterspringdatajpawithhibernate/MasterSpringDataJpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.masterspringdatajpawithhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringDataJpaWithHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringDataJpaWithHibernateApplication.class, args);
+    }
+
+}
---
+++ spring/master-spring-data-jpa-with-hibernate/src/test/java/com/todorkrastev/masterspringdatajpawithhibernate/MasterSpringDataJpaWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.masterspringdatajpawithhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringDataJpaWithHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
